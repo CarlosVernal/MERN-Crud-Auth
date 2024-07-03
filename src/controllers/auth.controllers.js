4e1310d7ce9bd21ee2889c5709a7e8629bd3a35d
Carlos Vernal <carlos.vernal.navarrete@gmail.com>
2024-07-02 22:02:03 -0400

    Se crea Clave "secreta" para tokens, encriptacion de la contraseña y se crea funcion para generar tokens (_id).

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -1,19 +1,32 @@
-import e from "express";
 import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs"; //Modulo para encriptar contraseñas
+import { createAccessToken } from "../libs/jwt.js";
 
 export const register = async (req, res) => {
   const { username, email, password } = req.body;
   try {
+    const passwordHash = await bcryptjs.hash(password, 10); //Convertir a un string aleatorio
     const newUser = new User({
+      //Crea el usuario con la contraseña enciptada
       username,
       email,
-      password,
+      password: passwordHash,
     });
-    const userSaved = await newUser.save();
-    res.json(userSaved)
+
+    const userSaved = await newUser.save(); // Guarda el usuario en DB y se deja una copia de los datos en userSaved
+    const token = await createAccessToken({ id: userSaved._id }); // se crea un token
+
+    res.cookie("token", token); //establece el token en una cookie para pasarla al frontEnd
+    res.json({
+      //envio de datos del usuario creado al frontEnd
+      id: userSaved._id,
+      username: userSaved.username,
+      email: userSaved.email,
+      createdAt: userSaved.createdAt,
+      updatedAt: userSaved.updatedAt,
+    });
   } catch (error) {
-    console.log(error);
+    res.status(500).json({ message: error.message });
   }
 };
-
 export const login = (req, res) => res.send("login");
